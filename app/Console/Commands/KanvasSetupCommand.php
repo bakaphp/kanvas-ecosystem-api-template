e9e5a0dc61ba684810eee2a31d08d6b71541953a
Rafael White <rwhite@mctekk.com>
2023-06-16 11:49:56 -0400

    removed migration of kanvas packages

--- app/Console/Commands/KanvasSetupCommand.php
+++ app/Console/Commands/KanvasSetupCommand.php
@@ -38,14 +38,10 @@ class KanvasSetupCommand extends Command
 
         $commands = [
             'migrate',
-            'migrate --path database/migrations/Inventory/ --database inventory',
-            'migrate --path database/migrations/Social/ --database social',
-            'migrate --path database/migrations/Guild/ --database crm',
             'db:seed',
             'kanvas:create-role Admin',
             'kanvas:create-role Users',
             'kanvas:create-role Agents',
-            'kanvas:filesystem-setup',
         ];
 
         foreach ($commands as $command) {
